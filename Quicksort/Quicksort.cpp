a0baa5a5c66463f9b18bf91389c14ef1d30ee3b3
muhfarrasm <muhammadfarrasm@gmail.com>
2023-03-30 12:48:09 +0700

    fix error

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -98,11 +98,11 @@ void display() {
 
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[1] << " ";
+		cout << arr[i] << " ";
 
 	}
 	cout << "\n\nNumber of comparasions:  " << cmp_count << endl;
-	cout << "\n\nNumber of data movements:  " << mov_count << endl;
+	cout << "Number of data movements:  " << mov_count << endl;
 }
 
 int main()
